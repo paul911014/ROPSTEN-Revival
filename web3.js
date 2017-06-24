13853778195a90c531f2712e68e7b43a54618e0a
CrashOverride <Cr4shOverride@gmx.de>
2017-06-24 18:05:50 +0200

    Update web3.js

--- web3.js
+++ web3.js
@@ -1,6 +1,6 @@
 https://github.com/shayanb/web3.js#develop
 
-/*explanation of javascript api relations.
+explanation of javascript api relations.
 
 note the last Requirements state on the site:
 
@@ -11,4 +11,4 @@ npm
 sudo apt-get update
 sudo apt-get install nodejs
 sudo apt-get install npm
-sudo apt-get install nodejs-legacy  */
+sudo apt-get install nodejs-legacy 
