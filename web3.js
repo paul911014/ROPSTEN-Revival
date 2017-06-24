3b20b05bd0a0912bbe9522c3bf0fab091440a532
CrashOverride <Cr4shOverride@gmx.de>
2017-06-24 18:05:19 +0200

    Create web3.js

---
+++ web3.js
@@ -0,0 +1,14 @@
+https://github.com/shayanb/web3.js#develop
+
+/*explanation of javascript api relations.
+
+note the last Requirements state on the site:
+
+Requirements
+
+Node.js
+npm
+sudo apt-get update
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo apt-get install nodejs-legacy  */
